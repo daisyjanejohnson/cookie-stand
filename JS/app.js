111be7f4ee9b62cf7b4e3e0e998441e8c493e558
Daisy Johnson <daisyjane.johnson@gmail.com>
2020-05-04 19:39:57 -0600

    added HTML structure to sales page

---
+++ JS/app.js
@@ -0,0 +1,24 @@
+// Goal: 
+
+// Make object literals for each shop location
+
+var seattle = {
+  location: 'Seattle',
+  minCust: 23,
+  maxCust: 65,
+  avgCookie: 6.3,
+};
+
+var tokyo = {
+  location: 'Tokyo',
+  minCust: 3,
+  maxCust: 24,
+  avgCookie: 1.2,
+};
+
+var dubai = {
+  location: 'Dubai',
+  minCust: 11,
+  maxCust: 38,
+  avgCookie: 3.7,
+};
